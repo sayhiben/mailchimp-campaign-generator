b7a7e98128367d029fe26eee8cb1f6873ab6bc64
Ben M <bmenesini@gmail.com>
2017-03-04 16:11:33 -0800

    Makes segmentation and scheduling optional

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require 'yaml'
 require 'gibbon'
 require 'awesome_print'
 require 'active_support/time'
+require 'active_support/core_ext/object/blank'
 
 options = YAML::load_file('.config')
 
@@ -11,14 +12,11 @@ options['campaign_count'].times do |i|
   date = DateTime.now.beginning_of_day + (options['start_day'] + i)
   begin
     puts "Creating Campaign #{i}"
-    response = gibbon.campaigns.create(
+    campaign_options = {
       body: {
         type: 'regular',
         recipients: {
-          list_id: options['list_id'],
-          segment_opts: {
-            saved_segment_id: options['segment_id']
-          },
+          list_id: options['list_id']
         },
         settings: {
           subject_line: "#{options['subject_prefix']} - #{date.strftime('%A, %B %e, %Y')}",
@@ -26,7 +24,13 @@ options['campaign_count'].times do |i|
           reply_to: options['reply_to']
         }
       }
-    )
+    }
+    unless options['segment_id'].blank?
+      campaign_options[:body][:recipients].merge!(
+        segment_opts: { saved_segment_id: options['segment_id'] }
+      )
+    end
+    response = gibbon.campaigns.create(campaign_options)
     campaign_id = response.body['id']
 
     puts "Updating Campaign to use Template #{options['template_id']}"
@@ -38,13 +42,15 @@ options['campaign_count'].times do |i|
       }
     )
 
-    send_time = date + options['send_time'].hours
-    puts "Scheduling Campaign #{i} for #{send_time}"
-    gibbon.campaigns(campaign_id).actions.schedule.create(
-      body: {
-        schedule_time: send_time
-      }
-    )
+    unless options['send_time'].blank?
+      send_time = date + options['send_time'].hours
+      puts "Scheduling Campaign #{i} for #{send_time}"
+      gibbon.campaigns(campaign_id).actions.schedule.create(
+        body: {
+          schedule_time: send_time
+        }
+      )
+    end
   rescue Gibbon::MailChimpError => e
     puts "MailChimp Error: #{e.message} - #{e.raw_body}"
   end
